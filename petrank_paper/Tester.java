5d09fac21755cff0d4d88d039f4554a8001cd9b4
architaagarwal <saggiarchi@gmail.com>
2016-06-03 19:32:45 -0400

    Fixed the bug. Code crashes sometimes in Test5 - throws NullPointerException

--- petrank_paper/Tester.java
+++ petrank_paper/Tester.java
@@ -4,30 +4,35 @@ import java.util.ArrayList;
 
 public class Tester {
 
-	private static final int NUMTHREADS = 4;
-	private static Threads[] threadObj = new Threads[NUMTHREADS];
+	private static final int NUM_UPDATORS = 4;
+	private static final int NUM_ITERATORS = 5;
+	private static Threads[] threadUpdators = new Threads[NUM_UPDATORS];
+	private static Threads[] threadIterators = new Threads[NUM_ITERATORS];
 	private static ListIT listIT = new ListIT();
 
 	// following functions are creating threads which will call specific functions when run
-	public static void createThreadsForInsert(int i, List<Integer> listInteger) {
-		threadObj[i] = new Threads(Integer.toString(i), i, Threads.testInsert, listIT, listInteger);
+	public static Threads createThreadsForInsert(int i, List<Integer> listInteger) {
+		return (new Threads(Integer.toString(i), i, Threads.testInsert, listIT, listInteger));
 	}
 
-	public static void createThreadsForDelete(int i, List<Integer> listInteger) {
-		threadObj[i] = new Threads(Integer.toString(i), i, Threads.testDelete, listIT, listInteger);
+	public static Threads createThreadsForDelete(int i, List<Integer> listInteger) {
+		return (new Threads(Integer.toString(i), i, Threads.testDelete, listIT, listInteger));
 	}
 		
+	public static Threads createThreadsForIterator(int i) {
+		return (new Threads(Integer.toString(i), i, Threads.testIterator, listIT, new ArrayList<Integer>()));
+	}
 	
-	public static void runThreads() {
-		for (int i = 0; i < NUMTHREADS; i++) {
-			threadObj[i].start();
+	public static void runUpdators() {
+		for (int i = 0; i < NUM_UPDATORS; i++) {
+			threadUpdators[i].start();
 		}
 	}
 
-	public static void waitForThreads() {
-		for (int i = 0; i < NUMTHREADS; i++) {
+	public static void waitForUpdators() {
+		for (int i = 0; i < NUM_UPDATORS; i++) {
 			try {
-				threadObj[i].join();
+				threadUpdators[i].join();
 			} catch (InterruptedException e) {
 				System.out.println("Could not join");
 			}
@@ -38,59 +43,109 @@ public class Tester {
 		// iterate over the list
 		List<Integer> iterationNodes = new<Integer> ArrayList();
 		iterationNodes = list.iterate(0);
-
+		System.out.println("Iteration");
 		for(Integer element : iterationNodes) {
 			Integer key = element;
-			System.out.println(key);
+			System.out.print(key + " ");
 		}
+		System.out.println("");
 	}
 	
 	public static void main(String[] args)
 	{
 		List<Integer> listInteger = new ArrayList<Integer>();
-		/*for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < 10; i++) {
 			listInteger.add(i);
 		}
 		
 		// Test1: Do some insertion : Every thread tries to insert the same value
-		for (int i = 0; i < NUMTHREADS; i++) {
-		    createThreadsForInsert(i, listInteger);
+		System.out.println("**Test 1**");
+		for (int i = 0; i < NUM_UPDATORS; i++) {
+			threadUpdators[i] = createThreadsForInsert(i, listInteger);
 		}
-		runThreads();
-		waitForThreads();
+		runUpdators();
+		waitForUpdators();
 		System.out.println("Size = " + listIT.size(0));
 		iterate(listIT);
 		
 		// Test2: Do some deletion : Every thread tries to delete the same value
-		for (int i = 0; i < NUMTHREADS; i++) {
-		    createThreadsForDelete(i, listInteger);
+		System.out.println("\n**Test 2**");
+		for (int i = 0; i < NUM_UPDATORS; i++) {
+			threadUpdators[i] = createThreadsForDelete(i, listInteger);
 		}
-		runThreads();
-		waitForThreads();
+		runUpdators();
+		waitForUpdators();
 		System.out.println("Size = " + listIT.size(0));
 		iterate(listIT);
-		*/
+		
 		
 		//Test3: Insertion : All threads insert different values
-		for (int i = 0; i < NUMTHREADS; i++) {
+		System.out.println("\n**Test 3**");
+		for (int i = 0; i < NUM_UPDATORS; i++) {
 			listInteger.clear();
 			for(int j = 1; j <= 5; j++) {
 				listInteger.add(5 * i + j);
 			}
-		    createThreadsForInsert(i, listInteger);
-		    /*
-		    threadObj[i].start();
-		    try {
-				threadObj[i].join();
-			} catch (InterruptedException e) {
-				System.out.println("Could not join");
-			}
-		    System.out.println("Size = " + listIT.size(1));
-			iterate(listIT);	*/
+			threadUpdators[i] = createThreadsForInsert(i, new ArrayList<Integer>(listInteger));
 		}
-		runThreads();
-		waitForThreads();
+		runUpdators();
+		waitForUpdators();
 		System.out.println("Size = " + listIT.size(1));
 		iterate(listIT);
+		
+		//Test4 Deletion: All threads delete different values
+		System.out.println("\n**Test 4**");
+		for (int i = 0; i < NUM_UPDATORS; i++) {
+			listInteger.clear();
+			for(int j = 1; j <= 5; j++) {
+				listInteger.add(5 * i + j);
+			}
+			threadUpdators[i] = createThreadsForDelete(i, new ArrayList<Integer>(listInteger));
+		}
+		runUpdators();
+		waitForUpdators();
+		System.out.println("Size = " + listIT.size(1));
+		iterate(listIT);
+		
+		// Test 5: Do insertions and iterations together
+		System.out.println("**Test 5**");
+		
+		// Create multiple threads: multiple trying to insert and others iterating
+		// insert threads
+		for (int i = 0; i < NUM_UPDATORS; i++) {
+			listInteger.clear();
+			for(int j = i; j <= 20; j++) {
+				listInteger.add(20 * i + j);
+			}
+		    threadUpdators[i] = createThreadsForInsert(i, new ArrayList<Integer>(listInteger));
+		}
+		
+		// iterator threads
+		for (int i = 0; i < NUM_ITERATORS; i++) {
+			threadIterators[i] = createThreadsForIterator(i + NUM_UPDATORS);
+		}
+		// start insertion by half the threads
+		int k = 0;
+		for (k = 0; k < NUM_UPDATORS ; k++) {
+		    threadUpdators[k].start();
+		}
+		// start iteration by all the iterators
+		int m = 0;
+		for (m = 0; m < NUM_ITERATORS; m++) {
+			threadIterators[m].start();
+		}
+		/*
+		 // uncomment if some sort of interleaving between the 
+		 // starting of iterators and updators is desired
+		
+		// start insertion by rest of the threads
+		for (; k < NUM_UPDATORS; k++) {
+			threadUpdators[k].start();
+		}
+		for (; m < NUM_ITERATORS; m++) {
+			threadIterators[m].start();
+		}*/
+		//waitForUpdators();
+		//iterate(listIT);
 	}
 }
--- petrank_paper/Threads.java
+++ petrank_paper/Threads.java
@@ -3,57 +3,75 @@ import java.util.ArrayList;
 import java.util.function.BiFunction;
 
 class Threads extends Thread {
-   // thread properties
-   private String threadName;  
-   public int tid;
-   
-   // list on which thread operates on
-   public ListIT list;
-   
-   // function which threads execute when being run
-   // function accepts two arguments: Threads object and a list
-   // list contains the items that are to be inserted or deleted from ListIT list
-   public Object output;
-   BiFunction<Threads, List<Integer>, Object> f;
-   // this is the list that f will work on
-   List<Integer> listInteger = new ArrayList<Integer>();
-   
-   // sort of a function pointer
-   public static BiFunction<Threads, List<Integer>, Object> testInsert = (Threads t, List<Integer> l) -> {
-	   for(Integer element : l) {
-		   t.list.insert(t.tid, element);
+	// thread properties
+	private String threadName;  
+	public int tid;
+
+	// list on which thread operates on
+	public ListIT list;
+
+	// function which threads execute when being run
+	// function accepts two arguments: Threads object and a list
+	// list contains the items that are to be inserted or deleted from ListIT list
+	public Object output;
+	BiFunction<Threads, List<Integer>, Object> f;
+	// this is the list that f will work on
+	List<Integer> listInteger = new ArrayList<Integer>();
+
+	// sort of a function pointer
+	public static BiFunction<Threads, List<Integer>, Object> testInsert = (Threads t, List<Integer> l) -> {
+		for(Integer element : l) {
+			//System.out.println("Inserting "+ element);
+			t.list.insert(t.tid, element);
 		}
-	   return true; 
-   };
-   
-   // sort of a function pointer
-   public static BiFunction<Threads, List<Integer>, Object> testDelete = (Threads t, List<Integer> l) -> {   
-	   for(Integer element : l) {
-		   t.list.delete(t.tid, element);
+		return true; 
+	};
+
+	// sort of a function pointer
+	public static BiFunction<Threads, List<Integer>, Object> testDelete = (Threads t, List<Integer> l) -> {   
+		for(Integer element : l) {
+			t.list.delete(t.tid, element);
 		}
-	   return true; 
-   };
-   
-   public Threads(String name, int threadID, BiFunction<Threads, List<Integer>, Object> f, ListIT listIterator, List<Integer> listInteger) {
-	   threadName = name;
-	   this.tid = threadID;
-	   this.f = f;
-	   this.list = listIterator;
-	   this.listInteger = listInteger;
-   }
-   
-   /*
+		return true; 
+	};
+
+	// List l is dummy. Not used
+	public static BiFunction<Threads, List<Integer>, Object> testIterator = (Threads t, List<Integer> l) -> {
+		List<Integer> iterationNodes = new<Integer> ArrayList();
+		iterationNodes = t.list.iterate(t.tid);
+		
+		StringBuilder finalString = new StringBuilder();
+		finalString.append("Iteration by thread ");
+		finalString.append(t.threadName + " : ");
+		for(Integer element : iterationNodes) {
+			finalString.append(element + " ");
+		}
+		System.out.println(finalString);
+		return true; 
+	};
+
+	public Threads(String name, int threadID, BiFunction<Threads, List<Integer>, Object> f, ListIT listIterator, List<Integer> listInteger) {
+		this.threadName = name;
+		this.tid = threadID;
+		this.f = f;
+		this.list = listIterator;
+		this.listInteger = listInteger;
+	}
+
+	/*
    public static Function<Threads, Object> test2(int magic) {
 	   return (Threads t) -> {
 		   t.list.insert(t.tid, magic);
 		   return magic;
 	   };
    }
-   */
-   
-   public void run() {
-	  System.out.println("Running thread" + this.tid);
-      output = f.apply(this, this.listInteger);
-      System.out.println("Exiting thread" + this.tid);
-   }
+	 */
+
+	public void run() {
+		//System.out.println("Running thread" + this.tid);
+		//System.out.println(this + " ooooo " + this.list);
+		//System.out.println(this.listInteger);
+		output = f.apply(this, this.listInteger);
+		//System.out.println("Exiting thread" + this.tid);
+	}
 }
