a8b5cd9114128dd14efed9db96e98aa94d0b7317
Vikram Saraph <vsaraph@cs.brown.edu>
2016-07-09 21:24:22 -0400

    multiple runs per test

--- iterators/runTests.py
+++ iterators/runTests.py
@@ -7,7 +7,11 @@ DURATION = [2, 4]
 PERCENTAGES = [(25, 25, 50), (20, 10, 70), (50, 50, 0)]
 KEY_RANGE = [4096]
 INIT_SIZE = [1024]
+runs = 10
 
+# Maybe sanitize inputs here
+
+# Write configurations used to a file
 configfile = open("config.txt", 'w')
 configfile.write(' '.join(map(str, ITERATORS_NUM)) + '\n')
 configfile.write(' '.join(map(str, UPDATERS_NUM)) + '\n')
@@ -17,11 +21,15 @@ configfile.write(' '.join(map(str, KEY_RANGE)) + '\n')
 configfile.write(' '.join(map(str, INIT_SIZE)) + '\n')
 configfile.close()
 
+# Open file, write header
 outputfile = open("output.txt", 'w')
 outputfile.write("IT\tUP\tTIME\tCFG\tKEYR\tINIT\tHASH\tUBST\n")
+verbose = open("output_verbose.txt", 'w')
+verbose.write("IT\tUP\tTIME\tCFG\tKEYR\tINIT\tRUN\tHASH\tUBST\n")
 
 PARAMETER_COMBINATIONS = [ITERATORS_NUM, UPDATERS_NUM, DURATION, PERCENTAGES, KEY_RANGE, INIT_SIZE]
 
+# Iterate through all combinations
 def makeargs(param, alg, i):
 	args = ["java", "-cp", ("%s:%s/lib/java-getopt-1.0.13.jar") % (alg, alg), "IteratorTest"]
 	args += ["-i", str(i)]
@@ -33,25 +41,46 @@ def makeargs(param, alg, i):
 	args += ["-s", str(param[5])]
 	return args
 
+# for keeping track of progress
 count = 0
-total = len([j for j in itertools.product(*PARAMETER_COMBINATIONS)])
+total = runs * len([j for j in itertools.product(*PARAMETER_COMBINATIONS)])
 
+# main loop
 for param in itertools.product(*PARAMETER_COMBINATIONS):
-	pTest0h = Popen(makeargs(param, "hash", 0), stdout=PIPE)
-	result0h = int(pTest0h.communicate()[0].strip())
-	pTest1h = Popen(makeargs(param, "hash", param[0]), stdout=PIPE)
-	result1h = int(pTest1h.communicate()[0].strip())
+	accum_hash = 0
+	accum_ubst = 0
+	for r in xrange(runs):
+		# Compare each run against identical run with no iterators
+		# hash table
+		pTest0h = Popen(makeargs(param, "hash", 0), stdout=PIPE)
+		result0h = int(pTest0h.communicate()[0].strip())
+		pTest1h = Popen(makeargs(param, "hash", param[0]), stdout=PIPE)
+		result1h = int(pTest1h.communicate()[0].strip())
 
-	pTest0b = Popen(makeargs(param, "ubst", 0), stdout=PIPE)
-	result0b = int(pTest0b.communicate()[0].strip())
-	pTest1b = Popen(makeargs(param, "ubst", param[0]), stdout=PIPE)
-	result1b = int(pTest1b.communicate()[0].strip())
+		# unbalanced binary search tree
+		pTest0b = Popen(makeargs(param, "ubst", 0), stdout=PIPE)
+		result0b = int(pTest0b.communicate()[0].strip())
+		pTest1b = Popen(makeargs(param, "ubst", param[0]), stdout=PIPE)
+		result1b = int(pTest1b.communicate()[0].strip())
 
+		# calculate/write verbose output
+		line = reduce(lambda x, y: x + y, map(lambda x: str(x) + '\t', param + (r+1,)))
+		line += str(float(result1h)/result0h) + '\t'
+		line += str(float(result1b)/result0b) + '\n'
+		verbose.write(line)
+
+		# accumulate to calculate an average over runs
+		accum_hash += float(result1h) / result0h
+		accum_ubst += float(result1b) / result0b
+
+		count += 1
+		print "%d of %d done" % (count, total)
+
+	# write averages
 	line = reduce(lambda x, y: x + y, map(lambda x: str(x) + '\t', param))
-	line += str(float(result1h)/result0h) + '\t'
-	line += str(float(result1b)/result0b) + '\n'
+	line += str(accum_hash/runs) + '\t'
+	line += str(accum_ubst/runs) + '\n'
 	outputfile.write(line)
-	count += 1
-	print "%d of %d done" % (count, total)
 
 outputfile.close()
+verbose.close()
