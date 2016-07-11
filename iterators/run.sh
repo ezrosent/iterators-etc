610431b7b6da7e42a7ed864419f93210ff436983
Archita Agarwal <archita@cs.brown.edu>
2016-07-11 13:42:39 -0400

    Fixed some bugs in run.sh

--- iterators/run.sh
+++ iterators/run.sh
@@ -1,7 +1,8 @@
 cd hash
 javac -cp .:lib/java-getopt-1.0.13.jar IteratorTest.java
-cd ubst
+cd ../ubst
 javac -cp .:lib/java-getopt-1.0.13.jar IteratorTest.java
+cd ..
 python runTests.py
 gnuplot plot.gp.sh 
 
